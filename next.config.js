7b38fda7529f040f5a9a35033074ee1dc249d4fb
earlyspark <rainfulljar@yahoo.com>
2025-06-20 20:49:22 -0700

    Skip type checking and linting during build to reduce memory usage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   swcMinify: false,
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   experimental: {
     workerThreads: false,
     cpus: 1
